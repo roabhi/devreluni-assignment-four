55a610df868a0a5db70038db0535a601fdf17336
roabhi <teskostudio@gmail.com>
2024-11-19 16:06:49 +0100

    Removed some placeholder elements. Customized UI a bit

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,21 +2,12 @@
 
 import Image from "next/image";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
-          <p className="my-2 font-medium">Connected Address:</p>
-          <Address address={connectedAddress} />
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12 flex flex-col justify-center items-center">
+      <div className="flex items-center flex-col flex-grow">
+        <div className="flex-grow bg-base-300 w-full px-8 py-12 flex flex-col justify-center items-center">
           {/* Logo Image */}
           <div className="flex justify-center mb-4">
             <Image
